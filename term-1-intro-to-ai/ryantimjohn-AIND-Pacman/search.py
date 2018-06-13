b43186a3e1cd4c0b150438925ffa5052e2a48337
ryantimjohn <ryantimjohn@gmail.com>
2018-06-13 16:10:48 -0500

    Pacman final commit

--- term-1-intro-to-ai/ryantimjohn-AIND-Pacman/search.py
+++ term-1-intro-to-ai/ryantimjohn-AIND-Pacman/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -86,18 +86,115 @@ def depthFirstSearch(problem):
     print "Is the start a goal?", problem.isGoalState(problem.getStartState())
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from game import Directions
+
+    directionDict = {'North':Directions.NORTH, 'South':Directions.SOUTH,
+                     'East':Directions.EAST, 'West':Directions.WEST}
+    frontier = util.Stack()
+    visited = []
+    node = {}
+    node["parent"] = None
+    node["action"] = None
+    node["state"] = problem.getStartState()
+    node["cost"] = 0
+    visited.append(node["state"])
+    frontier.push(node)
+
+    def search(frontier, visited):
+        if not frontier.isEmpty():
+            current = frontier.pop()
+            if problem.isGoalState(current["state"]):
+                queue = util.Queue()
+                while current["parent"]:
+                    queue.push(current["action"])
+                    current = current["parent"]
+                return queue.list
+            for next in problem.getSuccessors(current["state"]):
+                if next[0] not in visited:
+                    node = {}
+                    node["parent"] = current
+                    node["action"] = directionDict[next[1]]
+                    node["state"] = next[0]
+                    node["cost"] = current["cost"] + next[2]
+                    visited.append(node["state"])
+                    frontier.push(node)
+            return search(frontier, visited)
+
+    return search(frontier, visited)
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from game import Directions
+
+    directionDict = {'North':Directions.NORTH, 'South':Directions.SOUTH,
+                     'East':Directions.EAST, 'West':Directions.WEST}
+    frontier = util.Queue()
+    visited = []
+    node = {}
+    node["parent"] = None
+    node["action"] = None
+    node["state"] = problem.getStartState()
+    node["cost"] = 0
+    visited.append(node["state"])
+    frontier.push(node)
+
+    def search(frontier, visited):
+        if not frontier.isEmpty():
+            current = frontier.pop()
+            if problem.isGoalState(current["state"]):
+                queue = util.Queue()
+                while current["parent"]:
+                    queue.push(current["action"])
+                    current = current["parent"]
+                return queue.list
+            for next in problem.getSuccessors(current["state"]):
+                if next[0] not in visited:
+                    node = {}
+                    node["parent"] = current
+                    node["action"] = directionDict[next[1]]
+                    node["state"] = next[0]
+                    node["cost"] = current["cost"] + next[2]
+                    visited.append(node["state"])
+                    frontier.push(node)
+            return search(frontier, visited)
+
+    return search(frontier, visited)
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from game import Directions
+
+    directionDict = {'North':Directions.NORTH, 'South':Directions.SOUTH,
+                     'East':Directions.EAST, 'West':Directions.WEST}
+    frontier = util.PriorityQueue()
+    visited = []
+    node = {}
+    node["parent"] = None
+    node["actions"] = []
+    node["state"] = problem.getStartState()
+    node["cost"] = 0
+    visited.append(node["state"])
+    frontier.push(node, node["cost"])
+
+    def search(frontier, visited):
+        if not frontier.isEmpty():
+            current = frontier.pop()
+            if problem.isGoalState(current["state"]):
+                return current["actions"]
+            for next in problem.getSuccessors(current["state"]):
+                if next[0] not in visited:
+                    node = {}
+                    node["parent"] = current
+                    node["actions"] = current["actions"] + [directionDict[next[1]]]
+                    node["state"] = next[0]
+                    node["cost"] = problem.getCostOfActions(node["actions"])
+                    visited.append(node["state"])
+                    frontier.push(node, node["cost"])
+            return search(frontier, visited)
+
+    return search(frontier, visited)
 
 def nullHeuristic(state, problem=None):
     """
@@ -108,7 +205,38 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
+    from game import Directions
+
+    directionDict = {'North':Directions.NORTH, 'South':Directions.SOUTH,
+                     'East':Directions.EAST, 'West':Directions.WEST}
+    frontier = util.PriorityQueue()
+    visited = []
+    node = {}
+    node["parent"] = None
+    node["actions"] = []
+    node["state"] = problem.getStartState()
+    node["cost"] = 0
+    visited.append(node["state"])
+    frontier.push(node, node["cost"])
+
+    def search(frontier, visited):
+        if not frontier.isEmpty():
+            current = frontier.pop()
+            if problem.isGoalState(current["state"]):
+                return current["actions"]
+            for next in problem.getSuccessors(current["state"]):
+                if next[0] not in visited:
+                    node = {}
+                    node["parent"] = current
+                    node["actions"] = current["actions"] + [directionDict[next[1]]]
+                    node["state"] = next[0]
+                    node["cost"] = (problem.getCostOfActions(node["actions"]) +
+                                    heuristic(node["state"], problem))
+                    visited.append(node["state"])
+                    frontier.push(node, node["cost"])
+            return search(frontier, visited)
+
+    return search(frontier, visited)
     util.raiseNotDefined()
 
 
--- term-1-intro-to-ai/ryantimjohn-AIND-Pacman/searchAgents.py
+++ term-1-intro-to-ai/ryantimjohn-AIND-Pacman/searchAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -294,15 +294,21 @@ class CornersProblem(search.SearchProblem):
         Returns the start state (in your state space, not the full Pacman state
         space)
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.startingPosition
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if state in self.corners:
+            self._visitedlist.append(state)
+
+        numCorners = 4
+        for corner in self.corners:
+            if corner in self._visitedlist:
+                numCorners -= 1
+
+        return numCorners == 0
 
     def getSuccessors(self, state):
         """
@@ -324,7 +330,13 @@ class CornersProblem(search.SearchProblem):
             #   nextx, nexty = int(x + dx), int(y + dy)
             #   hitsWall = self.walls[nextx][nexty]
 
-            "*** YOUR CODE HERE ***"
+            x, y = state
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            if not self.walls[nextx][nexty]:
+                nextState = (nextx, nexty)
+                cost = 1
+                successors.append( ( nextState, action, cost ) )
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
@@ -360,7 +372,8 @@ def cornersHeuristic(state, problem):
     walls = problem.walls # These are the walls of the maze, as a Grid (game.py)
 
     "*** YOUR CODE HERE ***"
-    return 0 # Default to trivial solution
+    return 0
+
 
 class AStarCornersAgent(SearchAgent):
     "A SearchAgent for FoodSearchProblem using A* and your foodHeuristic"
